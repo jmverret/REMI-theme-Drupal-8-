8ee8e4fac7f2021366e58d0565efb14b1e352a05
JMV <jeanmarieverret@gmail.com>
2017-08-25 12:11:26 +0200

    remove not working hover

--- includes/bootstrap/js/customhover.js
+++
@@ -1,7 +0,0 @@
-$('.dropdown').mouseenter(function(){
-    if(!$('.navbar-toggle').is(':visible')) { // disable for mobile view
-        if(!$(this).hasClass('open')) { // Keeps it open when hover it again
-            $('.dropdown-toggle', this).trigger('click');
-        }
-    }
-});
